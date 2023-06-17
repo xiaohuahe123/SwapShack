bfbec8b18a13a6f332be5531f777c51c19a68abd
yzhou626 <lovepeanut628@gmail.com>
2023-06-17 16:44:01 -0700

    backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const User = require('./config');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = 4000
+
+app.get('/', async (req, res) => {
+	const snapshot = await User.get();
+	const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+	res.send(list);
+});
+
+app.post('/create', async (req, res) => {
+	const data = req.body;
+	await User.add({ data });
+	res.send({ msg: 'User Added' });
+});
+
+
+
+app.listen(PORT, () => console.log(`Up & RUnning at http://localhost:${PORT}`));
