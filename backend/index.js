cacd6ad870f8b90210a71a9f0fbfd6b46da19c33
yzhou626 <lovepeanut628@gmail.com>
2023-06-17 18:15:36 -0700

    api

--- backend/index.js
+++ backend/index.js
@@ -19,4 +19,18 @@ app.post('/create', async (req, res) => {
 	res.send({ msg: 'User Added' });
 });
 
+pp.post('/update', async (req, res) => {
+	const id = req.body.id;
+	delete req.body.id;
+	const data = req.body;
+	await User.doc(id).update(data);
+	res.send({ msg: 'Updated' });
+});
+
+app.post('/delete', async (req, res) => {
+	const id = req.body.id;
+	await User.doc(id).delete();
+	res.send({ msg: 'Deleted' });
+});
+
 app.listen(PORT, () => console.log(`Up & RUnning at http://localhost:${PORT}`));
