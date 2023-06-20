8e15fdc8d3c620f2ac9844b548d37a7994d2263d
yzhou626 <lovepeanut628@gmail.com>
2023-06-19 18:41:45 -0700

    app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState,  Suspense } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 
 function App() {
