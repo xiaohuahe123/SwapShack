00e1dff33f14568e699ea6a31b84cd6ea4904647
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-06-21 14:31:26 -0700

    SubSection_js_coments

--- frontend/src/components/SubSection/SubSection.js
+++ frontend/src/components/SubSection/SubSection.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './SubSection.css';
 const SubSection = ({ section }) => {
 	return (
+		 // Main container for the SubSection component
 		<div className="subSection">
-			<div className="sub__container1">
+			{/* Container with background for the SubSection */}
+			<div className="sub__container1"> 
+
+			{/* Container for the content */}
 				<div className="container__content">
+
+					 {/* Paragraph displaying the section path */}
 					<p className="container__section">Home/{section}</p>
+
+					          {/* Heading displaying the section title */}
 					<h3 className="container__title">{section}</h3>
 				</div>
 			</div>
