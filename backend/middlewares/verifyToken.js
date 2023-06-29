313e5225b7879e8d4fa4e4918442b605ad40ba4e
yzhou626 <lovepeanut628@gmail.com>
2023-06-29 15:15:47 -0700

    auth middleware

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -18,4 +18,23 @@ const verifyToken = (req, res, next) => {
 	});
 };
 
+// Authentication middleware
+const authenticateToken = (req, res, next) => {
+	const authHeader = req.headers['authorization'];
+	const token = authHeader && authHeader.split(' ')[1];
+
+	if (!token) {
+		return res.status(401).json({ message: 'Missing token' });
+	}
+
+	jwt.verify(token, secretKey, (err, decoded) => {
+		if (err) {
+			return res.status(403).json({ message: 'Invalid token' });
+		}
+
+		req.userId = decoded.userId;
+		next();
+	});
+};
+
 module.exports = authenticateToken;
