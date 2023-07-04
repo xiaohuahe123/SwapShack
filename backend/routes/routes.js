4680fdd469bcc03140980d4d2b3f00bc90a3840a
yzhou626 <lovepeanut628@gmail.com>
2023-07-04 11:42:18 -0700

    routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,5 +4,7 @@ const authRouter = require("./auth.route");
 const routes = Router()
 
 routes.use('/auth', authRouter)
+routes.use('/post', postRouter)
+routes.use('/profile', profileRouter)
 
 module.exports  = routes
