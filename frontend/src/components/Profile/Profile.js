8ae6a680226b5ecd6e1405f3b2fde923f8a10101
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-07-05 15:34:28 -0700

    Profile_js

---
+++ frontend/src/components/Profile/Profile.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import BartContext from '../../store/bartContext';
+import SubSection from '../SubSection/SubSection';
+import './Profile.css';
+import api from '../../restClient/api';
+
+const Profile = () => {
+	const { isLoggedIn, user, token, setUser } = useContext(BartContext);
+	const [profile, setProfile] = useState(user);
+	const navigate = useNavigate();
+	useEffect(function () {
+		if (isLoggedIn !== true) {
+			return navigate('/login');
+		}
+		const headers = {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		};
+		try {
+			api.get(`/profile/${user.id}`, { headers }).then((res) => {
+				if (res.data) {
+					setUser(res.data);
+					setProfile(res.data);
+				}
+			});
+		} catch (error) {
+			console.log(error);
+			alert(error.message);
+		}
+	}, []);
+
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+
+		// Update the state by merging the updated value with the existing state
+		setProfile((prevObject) => ({
+			...prevObject,
+			[name]: value
+		}));
+	};
+	const handleProfileUpdate = async (e) => {
+		e.preventDefault();
+		try {
+			const headers = {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`
+			};
+			const response = await api.put(`/profile/${user.id}`, profile, { headers });
+			if (response && response.error) alert(response.error);
+			if (response && response.data) setUser(response.data);
+		} catch (err) {
+			console.log(err);
+			alert(err);
+		}
+	};
+	return (
+		<div>
+			<SubSection section="Profile" />
+			<div className="profile__container">
+				<div className="profile__subContainer">
+					<form onSubmit={handleProfileUpdate}>
+						<div className="profile__inputContainer">
+							<h3 className="profile__subTitle">Full Name</h3>
+							<input className="profile__input" name="fullName" value={user.fullName} onChange={handleInputChange} />
+						</div>
+						<div className="profile__inputContainer">
+							<h3 className="profile__subTitle">Phone</h3>
+							<input className="profile__input" name="phone" value={user.phone} onChange={handleInputChange} />
+						</div>
+						<div className="profile__inputContainer">
+							<h3 className="profile__subTitle">City</h3>
+							<input className="profile__input" name="city" value={user.city} onChange={handleInputChange} />
+						</div>
+						<div className="profile__inputContainer">
+							<h3 className="profile__subTitle">State/Province</h3>
+							<input className="profile__input" name="state" value={user.state} onChange={handleInputChange} />
+						</div>
+						<div className="profile__inputContainer">
+							<h3 className="profile__subTitle">Country</h3>
+							<input className="profile__input" name="country" value={user.country} onChange={handleInputChange} />
+						</div>
+						<div className="profile__inputContainer">
+							<h3 className="profile__subTitle">Email</h3>
+							<input disabled={true} className="profile__input" name="email" value={user.email} onChange={handleInputChange} />
+						</div>
+
+						<button className="profile__button">UPDATE</button>
+					</form>
+					<p></p>
+					<p></p>
+					<p></p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
