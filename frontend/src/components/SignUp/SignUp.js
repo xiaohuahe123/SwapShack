facf73505501a1a2799e6daf2e3ebf217adca73e
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-07-05 22:13:15 -0700

    SignUp_update

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -5,42 +5,26 @@ import BartContext from '../../store/bartContext';
 import SubSection from '../SubSection/SubSection';
 import './SignUp.css';
 const SignUp = () => {
-
-	// State variables to store user input
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-
-	// Navigation hook for redirecting
 	const navigate = useNavigate();
-
-	// Accessing the BartContext to check if the user is already logged in
 	const { isLoggedIn } = useContext(BartContext);
-
-	// Check if the user is already logged in and redirect to home if true
 	useEffect(() => {
 		if (isLoggedIn === true) {
 			navigate('/home');
 		}
 	}, []);
-
-	// Handle sign up form submission
 	const handleSignUp = async (e) => {
 		e.preventDefault();
 		try {
-
-			// Create a data object with user's email, password, and returnSecureToken flag
 			const data = {
 				email: email,
 				password: password,
 				returnSecureToken: true
 			};
-
-			// Make a POST request to the signup endpoint using axios
 			const response = await axios.post('/auth/signup',{ email, password }, {
 				headers: { 'Content-Type': 'application/json' }
 			});
-
-			// After successful signup, navigate to the homepage
 			if (response && response.error) alert(response.error);
 			if (response && response.data.message) navigate('/');
 		} catch (err) {
@@ -48,8 +32,6 @@ const SignUp = () => {
 			alert(err);
 		}
 	};
-
-	// JSX rendering, (JavaScript XML) representing structure of signup form and its related elements such as input fields, buttons and links.
 	return (
 		<div>
 			<SubSection section="Sign up" />
@@ -78,5 +60,4 @@ const SignUp = () => {
 	);
 };
 
-//Exporting the component to make it available for other files to import and use.
 export default SignUp;
