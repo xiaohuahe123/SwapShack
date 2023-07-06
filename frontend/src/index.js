dbb18b77c353f26262ee919636e5e0496376d979
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-07-06 12:59:19 -0700

    index_comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,17 @@ import App from './App';
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
+//creates a root for the React application by selecting an HTML element with the id 'root'.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//rendering the React application.
 root.render(
-  <BrowserRouter>
+
+  //define the structure and components of the application.
+
+  //BrowserRouter: to enable routing functionality for the entire application.
+  //React.StrictMode: to perform additional runtime checks and warnings during development.
+  <BrowserRouter> 
     <React.StrictMode>
       <App />
     </React.StrictMode>
