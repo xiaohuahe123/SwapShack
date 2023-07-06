c9c90bc1233e86f39c57bd5b12981fb0b39b32db
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-07-06 13:20:27 -0700

    Bart_comments

--- frontend/src/store/BartProvider.js
+++ frontend/src/store/BartProvider.js
@@ -11,36 +11,36 @@ import BartContext from './bartContext';
 
 const BartProvider = (props) => {
 	
-    const [showLogout, setShowLogout] = useState(false);
-    const logout = () => {
-		setToken(null);
-		setIsLoggedIn(false);
-		setShowLogout(false);
-		localStorage.removeItem('tokenId');
-		localStorage.removeItem('user');
+    const [showLogout, setShowLogout] = useState(false);  // State variable to manage the visibility of logout button
+    const logout = () => { // Function to handle the logout action
+		setToken(null);// Clear the token
+		setIsLoggedIn(false);  // Set isLoggedIn to false
+		setShowLogout(false); // Hide the logout button
+		localStorage.removeItem('tokenId'); // Remove the token from localStorage
+		localStorage.removeItem('user'); // Remove the user from localStorage
 	};
     
-	const [isLoggedIn, setIsLoggedIn] = useState(() => {
+	const [isLoggedIn, setIsLoggedIn] = useState(() => { // State variable to manage the login status
 		if (localStorage.getItem('tokenId')) {
-			return true;
+			return true; // If tokenId is present in localStorage, the user is logged in
 		} else {
-			return false;
+			return false; // Otherwise, the user is not logged in
 		}
 	});
-	const [token, setToken] = useState(() => {
-		return localStorage.getItem('tokenId') || null;
+	const [token, setToken] = useState(() => { // State variable to store the token
+		return localStorage.getItem('tokenId') || null; // Retrieve the token from localStorage or set it to null if not present
 	});
-	const [email, setEmail] = useState(() => {
+	const [email, setEmail] = useState(() => { // State variable to store the email
 		if (localStorage.getItem('userEmail')) {
-			return localStorage.getItem('userEmail');
+			return localStorage.getItem('userEmail'); // Retrieve the email from localStorage
 		} else {
-			return '';
+			return ''; // Set the email to an empty string if not present in localStorage
 		}
 	});
-	const login = (token, email) => {
-		setToken(token);
-		setEmail(email);
-		setIsLoggedIn(true);	
+	const login = (token, email) => { // Function to handle the login action
+		setToken(token); // Set the token
+		setEmail(email);  // Set the email
+		setIsLoggedIn(true);	// Set isLoggedIn to true
 	};
 
 	let bartValue = {
