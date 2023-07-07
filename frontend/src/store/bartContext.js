686ca8dc2a376b3b5b4fb1bec4471c286fa15bc3
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-07-06 20:48:40 -0700

    bart_update

--- frontend/src/store/bartContext.js
+++ frontend/src/store/bartContext.js
@@ -1,27 +1,15 @@
 import { createContext } from 'react';
 
 const BartContext = createContext({
-	products: [],
-	totalAmount: 0,
-	numberOfProduct: 0,
-	addProduct: (product) => {},
-	removeProduct: (id) => {},
-	removeAll: () => {},
-	incrementQuantity: (id) => {},
-	decrementQuantity: (id) => {},
-	token: '',
+
+	logout: () => {},
+	showLogout: false,
 	isLoggedIn: false,
 	login: (token, user) => {},
+	headers:{},
 	user: {},
 	setUser: () => {},
-	getUserCart: () => {},
-	data: [],
-	homeData: [],
-	searchProduct: '',
-	searchHandler: (currLocation) => {},
-	isFilter: false,
-	logout: () => {},
-	showLogout: false
+	
 });
 
 export default BartContext;
