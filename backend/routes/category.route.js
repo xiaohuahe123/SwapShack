7476a2575dca12cc68b5d0337d68a70817ea2a39
yzhou626 <lovepeanut628@gmail.com>
2023-07-12 16:08:02 -0700

    Backend_Category_API

--- backend/routes/category.route.js
+++ backend/routes/category.route.js
@@ -17,6 +17,51 @@ categoryRouter.get('/', async (req, res) => {
 	}
 });
 
+// Create a route to fetch the subCategories data based on the selected category
+categoryRouter.get('/:categoryId/subCategories', async (req, res) => {
+	try {
+		const categoryId = req.params.categoryId;
+		const subCategoriesSnapshot = await Categories.doc(categoryId).collection('subCategories').get();
+		const subCategoriesData = subCategoriesSnapshot.docs.map((doc) => ({
+			id: doc.id,
+			...doc.data()
+		}));
+		res.json(subCategoriesData);
+	} catch (error) {
+		console.error('Error fetching subCategories:', error);
+		res.status(500).json({ error: 'Error fetching subCategories' });
+	}
+});
 
+// Create a route to create a category
+categoryRouter.post('/', async (req, res) => {
+	try {
+		const { name } = req.body;
+
+		const categoryRef = await Categories.add({ name });
+		const newCategory = { id: categoryRef.id, name };
+
+		res.json(newCategory);
+	} catch (error) {
+		console.error('Error creating category:', error);
+		res.status(500).json({ error: 'Error creating category' });
+	}
+});
+
+// Create a route to create a subCategory
+categoryRouter.post('/:categoryId/subCategories', async (req, res) => {
+	try {
+		const categoryId = req.params.categoryId;
+		const { name } = req.body;
+
+		const subCategoryRef = await Categories.doc(categoryId).collection('subCategories').add({ name });
+		const newSubCategory = { id: subCategoryRef.id, name };
+
+		res.json(newSubCategory);
+	} catch (error) {
+		console.error('Error creating subCategory:', error);
+		res.status(500).json({ error: 'Error creating subCategory' });
+	}
+});
 
 module.exports = categoryRouter;
