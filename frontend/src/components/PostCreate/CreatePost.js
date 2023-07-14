da9fda1622b8819789850de529c60b51ae91263b
“saitejapatnam1” <saitejapatnam1@gmail.comgit config --global user.email saitejapatnam1@gmail.com>
2023-07-14 11:35:53 -0700

    CreatePost_js_update

--- frontend/src/components/PostCreate/CreatePost.js
+++ frontend/src/components/PostCreate/CreatePost.js
@@ -21,3 +21,52 @@ const CreatePost = ({ id, createPost, editPost }) => {
 		condition: '',
 		location: ''
 	});
+
+   
+	useEffect(() => {
+		if (isEditMode) {
+			
+			getPostById(id).then((data) => setPost({ ...data }));
+		}
+	}, [id, isEditMode]);
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setPost((prevState) => ({ ...prevState, [name]: value }));
+	};
+
+	const handleSelectionChange = (obj) => {
+		setPost((prevState) => ({ ...prevState, ...obj }));
+	};
+
+	const handleCreatePost = () => {
+		createPost(post);
+	};
+
+	const handleEditPost = () => {
+		editPost(id, post);
+	};
+
+	return (
+		<div className="create-post">
+			<h2>{isEditMode ? 'Edit Post' : 'Create Post'}</h2>
+			<input type="text" name="name" value={post.name} onChange={handleInputChange} placeholder="Name" className="create-post__input" />
+			<textarea name="description" value={post.description} onChange={handleInputChange} placeholder="Description" className="create-post__textarea" />
+			<input type="text" name="price" value={post.price} onChange={handleInputChange} placeholder="Price" className="create-post__input" />
+			<input type="text" name="condition" value={post.condition} onChange={handleInputChange} placeholder="Condition" className="create-post__input" />
+			<LocationDropdown onSelectionChange={handleSelectionChange} selectedCountryId={post.countryId} selectedStateId={post.stateId} selectedCityId={post.cityId} type={LOCATION} />
+			<LocationDropdown onSelectionChange={handleSelectionChange} selectedCategoryId={post.categoryId} selectedSubCategoryId={post.subCategoryId} type={CATEGORY} />
+			{isEditMode ? (
+				<button onClick={handleEditPost} className="create-post__button">
+					Save Changes
+				</button>
+			) : (
+				<button onClick={handleCreatePost} className="create-post__button">
+					Create
+				</button>
+			)}
+		</div>
+	);
+};
+
+export default CreatePost;
