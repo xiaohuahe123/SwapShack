e86ccdd58959ddc25df69ece529fb07b882dc9b5
yzhou626 <lovepeanut628@gmail.com>
2023-07-14 21:31:18 -0700

    post get and create API

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -10,4 +10,19 @@ async function getAllPosts() {
 	return posts;
 }
 
+// Get a single post by ID
+async function getPostById(id) {
+	const postDoc = await Posts.doc(id).get();
+	if (postDoc.exists) {
+		return { id: postDoc.id, ...postDoc.data() };
+	}
+	return null;
+}
+
+// Create a new post
+async function createPost(postData) {
+	const postRef = await Posts.add(postData);
+	return postRef.id;
+}
+
 module.exports = getAllPosts;
