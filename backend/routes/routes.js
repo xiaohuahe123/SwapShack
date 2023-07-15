d3b8be685c25ec109c142671fe7a6fe6ab5ff666
yzhou626 <lovepeanut628@gmail.com>
2023-07-14 21:34:55 -0700

    routes update

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const authRouter = require("./auth.route");
 const postRouter = require("./posts.route");
 const profileRouter = require("./profile.route");
+const categoryRouter = require("./category.route");
+const locationRouter = require("./location.route");
 
 const routes = Router()
 
 routes.use('/auth', authRouter)
 routes.use('/post', postRouter)
+routes.use('/category', categoryRouter)
+routes.use('/location', locationRouter)
 routes.use('/profile', profileRouter)
 
 module.exports  = routes
